048cecaa4219dc9576933ab97eab93cd368923a3
CluadiaSantana <is726396@iteso.mx>
2021-05-11 17:26:22 -0500

    c

--- Public/Login.js
+++ Public/Login.js
@@ -11,12 +11,13 @@ entrar.addEventListener("click", async function(e){
         "email": datos.querySelector('#eemail').value,
         "password": datos.querySelector('#inputPassword').value
     }
-    console.log(f);
+    let imp=JSON.stringify(f);
+    console.log(imp);
     let resp= await fetch("http://127.0.0.1:3000/api/User/Login",{
         method: 'POST',
         headers:{'Content-Type': 'application/json'},
-        //mode: 'no-cors',
-        body: JSON.stringify(f)
+        mode: 'no-cors',
+        body: imp
     });
     console.log(resp.status);
     if(resp.status==200){
