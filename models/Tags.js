3c05dd15ee620875e7cd3f63d858bc30d30d8cfa
Luis Nitro 5 <luis.ramos@juiceweb.mx>
2021-05-12 10:10:45 -0500

    ingredientes from db

--- models/Tags.js
+++ models/Tags.js
@@ -5,35 +5,18 @@ let tagSchema = mongoose.Schema({
         type: String,
         requiere: true
     }
-});
+})
 
-tagSchema.statics.savetag= async (newtag)=>{
-    let tag= new Tag(newtag);
-    console.log(newtag);
-    try{
-        let doc = await tag.save();
-        console.log(doc);
-        return doc;
-    }catch(e){
-        console.log("Error al guardar" ,e.code);
-        return {error: 'Fallo al guardar'};
-    }
-}
 
-tagSchema.statics.updateTag = async function(_id, tag ){
-    let doc = await Tag.findOneAndUpdate(
-           {_id},
-           {$set: tag },
-           {new: true, useFindAndModify: false} );
-    return doc;
-}
-
-tagSchema.statics.deleteTag = async function(_id ){
-    await Tag.findOneAndDelete(
-           {_id});
-    return;
+tagSchema.statics.getTag= async ()=>{
+    let docs = await Tag.find({});
+    console.log(docs);
+    return docs;
 }
 
 let Tag= mongoose.model('tag',tagSchema);
+
+//Tag.getTag();
+
 
 module.exports=Tag;
