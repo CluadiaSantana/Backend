03fabd6291ac9b7a8c1aa96dce534056fa70652f
CluadiaSantana <is726396@iteso.mx>
2021-05-15 19:38:14 -0500

    Commit TRES

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Ingrediente = require("./models/Ingredientes");
 const Utensilio = require("./models/Utensilios");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 
