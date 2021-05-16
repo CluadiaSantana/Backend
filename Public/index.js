397e6c95b870d406d3d0bd0603c1f2616926b8ae
CluadiaSantana <is726396@iteso.mx>
2021-05-15 22:19:30 -0500

    Commit claudiaaa

--- Public/index.js
+++ Public/index.js
@@ -86,18 +86,16 @@ async function buscar(e) {
 document.getElementById("buscar").addEventListener("click", buscar);
 
 window.onload = function () {
-  listing_ingredients();
-  listing_utensilios();
   console.log(sessionStorage.token);
-  if (sessionStorage.token) {
-    document.getElementById("Linkreg").classList.add("oculto");
+  // if (sessionStorage.token) {
+  //   document.getElementById("Linkreg").classList.add("oculto");
 
-    document.getElementById("login").innerText = "logout";
-  } else {
-    document.getElementById("Linkreg").classList.remove("oculto");
+  //   document.getElementById("login").innerText = "logout";
+  // } else {
+  //   document.getElementById("Linkreg").classList.remove("oculto");
 
-    document.getElementById("login").innerText = "login";
-  }
+  //   document.getElementById("login").innerText = "login";
+  // }
   document.getElementById("login").addEventListener("click", function () {
     sessionStorage.token = null;
     sessionStorage.us = null;
