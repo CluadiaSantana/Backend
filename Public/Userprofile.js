f669b1a91ce462147f1d15a1376fb863dcba05c3
CluadiaSantana <is726396@iteso.mx>
2021-05-15 21:22:56 -0500

    login y registro

--- Public/Userprofile.js
+++ Public/Userprofile.js
@@ -225,4 +225,17 @@ window.onload = function () {
   } else {
     document.getElementById("selectRol").classList.remove("oculto");
   }
+  if (sessionStorage.token) {
+    document.getElementById("Linkreg").classList.add("oculto");
+
+    document.getElementById("login").innerText = "logout";
+  } else {
+    document.getElementById("Linkreg").classList.remove("oculto");
+
+    document.getElementById("login").innerText = "login";
+  }
+  document.getElementById("login").addEventListener("click", function () {
+    sessionStorage.token = null;
+    sessionStorage.us = null;
+  });
 };
