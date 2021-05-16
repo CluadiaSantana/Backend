036893c02eda6f4df553444d14f7ca6006c23c51
Luis Nitro 5 <luis.ramos@juiceweb.mx>
2021-05-15 21:21:39 -0500

    se pueden dar de alta categorias e ingredientes crud completos

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const users = require("./users");
 const recipes = require("./recipes");
-const Categoria = require("./models/Categorias");
+const ingredientes = require("./ingredientes");
+const categorias = require("./categorias");
 const Tag = require("./models/Tags");
 var mongoose = require("./conexion");
 const { log, test } = require("./middlewares/logs");
 var Consignador = require("./models/User");
-const Ingrediente = require("./models/Ingredientes");
 const Utensilio = require("./models/Utensilios");
 const cors = require("cors");
 const app = express();
@@ -20,21 +20,24 @@ app.use(log, test);
 app.use(express.static(__dirname + "/public/index"));
 app.use("/api/User", users);
 app.use("/api/Recipe", recipes);
+app.use("/api/Ingrediente", ingredientes);
+app.use("/api/Categoria", categorias);
 
-app.get("/api/ingredientes", async (req, res) => {
-  let lista = await Ingrediente.getIngredientes();
-  res.send(lista);
-});
+
+// app.get("/api/ingredientes", async (req, res) => {
+//   let lista = await Ingrediente.getIngredientes();
+//   res.send(lista);
+// });
 
 app.get("/api/Utensilio", async (req, res) => {
   let lista = await Utensilio.getUtensilio();
   res.send(lista);
 });
 
-app.get("/api/Categoria" , async (req, res) => {
-  let lista= await Categoria.getCategoria();
-  res.send(lista);
-});
+// app.get("/api/Categoria" , async (req, res) => {
+//   let lista= await Categoria.getCategoria();
+//   res.send(lista);
+// });
 
 app.get("/api/Tag" , async (req, res) => {
   let lista= await Tag.getTag();
